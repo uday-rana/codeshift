c86788a13faf4598229fab576a822654cde3d683
Uday R <82779467+uday-rana@users.noreply.github.com>
2024-09-08 10:49:02 -0400

    Make -o a global option

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # codeshift
+
+Command-line tool that transforms source code files into any language.
+
+## Features
+
+- Streaming responses to stdout
+- Evaluates source files individually (Does not support shared context)
+
+## Installation
+
+- Install Node.js
+- Clone repo
+- Run `npm install`
+
+## Usage
+
+`codeshift [-o <output-filename>] <output-language> <input-files...>`
+
+### Options
+
+- `-o, --output`: Specify filename to write output to
+- `-h, --help`: Display help for a command
+- `-v, --version`: Output the version number
--- src/index.js
+++ src/index.js
@@ -11,31 +11,32 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 program
   .name("codeshift")
   .description("Transform code from one language to another")
-  .version(`v${version}`, "-v, --version");
+  .version(`v${version}`, "-v, --version")
+  .option("-o, --output <filename>", "specify filename to write output to");
 
 program
   .command("run", { isDefault: true })
   .description("default command")
-  .option("-o, --output <filename>", "specify filename to output to")
   .argument("<output-language>", "language to transform code to")
   .argument("<input-files...>", "source files to read")
-  .action(async (outputLang, inputFiles, options) => {
+  .action(async (outputLang, inputFiles) => {
+    const outputFile = program.opts().output;
+
     for (let filePath of inputFiles) {
       try {
         const fileContent = await fs.readFile(filePath, { encoding: "utf8" });
         const reponseStream = await getGroqChatStream(
           fileContent,
-          outputLang,
-          options
+          outputLang
         );
-        if (options.output) {
+        if (outputFile) {
           let response = "";
           for await (const chunk of reponseStream) {
             const chunkContent = chunk.choices[0]?.delta?.content || "";
             process.stdout.write(chunkContent);
             response += chunkContent;
           }
-          await fs.writeFile(options.output, response);
+          await fs.writeFile(outputFile, response);
         } else {
           for await (const chunk of reponseStream) {
             const chunkContent = chunk.choices[0]?.delta?.content || "";
@@ -55,7 +56,7 @@ async function getGroqChatStream(fileContent, outputLang) {
       {
         role: "system",
         content:
-          "You will receive source code files and must convert them to the desired language."
+          "You will receive source code files and must convert them to the desired language. Do not include any sentences in your response. Your response must consist entirely of the requested code. Do not enclose your response in a codeblock."
       },
       {
         role: "user",
